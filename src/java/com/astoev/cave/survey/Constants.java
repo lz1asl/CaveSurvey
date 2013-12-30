af1e62ef0a3f0dc1d4a00bbef233bf74bd7b898c
zmitrev <zhivko.mitrev@bianor.com>
2013-12-30 03:06:47 +0200

    Change date format not to include spaces

--- src/java/com/astoev/cave/survey/Constants.java
+++ src/java/com/astoev/cave/survey/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
     public static final String LOG_TAG_UI = "CaveUI";
     public static final String LOG_TAG_SERVICE = "SRVC";
 
-    public static final String DATE_FORMAT = "dd.MM.yyyy HH:mm:ss";
+    public static final String DATE_FORMAT = "yyyyMMdd_HHmmss";
 
     public static final String LEG_SELECTED = "leg";
     public static final String LEG_NOTE = "legNote";
