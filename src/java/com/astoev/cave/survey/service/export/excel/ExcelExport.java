63887cc8121be95e692f203a592fc9d843242d77
zmitrev <zhivko.mitrev@bianor.com>
2014-01-15 13:24:58 +0200

    Add all fields for gps/sketch/photo to excel. sketch and photo are now file links

--- src/java/com/astoev/cave/survey/service/export/excel/ExcelExport.java
+++ src/java/com/astoev/cave/survey/service/export/excel/ExcelExport.java
@@ -10,30 +10,47 @@ import com.astoev.cave.survey.model.*;
 import com.astoev.cave.survey.service.Options;
 import com.astoev.cave.survey.util.DaoUtil;
 import com.astoev.cave.survey.util.FileStorageUtil;
+import com.astoev.cave.survey.util.LocationUtil;
 import com.astoev.cave.survey.util.StringUtils;
-import org.apache.commons.io.IOUtils;
+import org.apache.poi.hssf.usermodel.HSSFHyperlink;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.List;
 
 /**
- * Created by IntelliJ IDEA.
+ * Exports the project's data as xsl file  
+ * 
  * User: astoev
  * Date: 2/12/12
  * Time: 7:25 PM
- * To change this template use File | Settings | File Templates.
+ * 
+ * @author astoev
+ * @author jmitrev
  */
 public class ExcelExport {
 
+    private static int CELL_FROM = 0;
+    private static int CELL_TO = 1;
+    private static int CELL_LENGHT = 2;
+    private static int CELL_AZIMUTH = 3;
+    private static int CELL_SLOPE = 4;
+    private static int CELL_LEFT = 5;
+    private static int CELL_RIGHT = 6;
+    private static int CELL_UP = 7;
+    private static int CELL_DOWN = 8;
+    private static int CELL_NOTE = 9;
+    private static int CELL_LATITUDE = 10;
+    private static int CELL_LONGITUDE = 11;
+    private static int CELL_ALTTITUDE = 12;
+    private static int CELL_ACCURACY = 13;
+    private static int CELL_DRAWING = 14;
+    private static int CELL_PHOTO = 15;
+    
     private Context mContext;
-
 
     public ExcelExport(Context aContext) {
         mContext = aContext;
@@ -50,39 +67,47 @@ public class ExcelExport {
             Sheet sheet = wb.createSheet(aProject.getName());
             Row headerRow = sheet.createRow(0);
             // header cells
-            Cell headerFrom = headerRow.createCell(0);
+            Cell headerFrom = headerRow.createCell(CELL_FROM);
             headerFrom.setCellValue(mContext.getString(R.string.main_table_header_from));
-            Cell headerTo = headerRow.createCell(1);
+            Cell headerTo = headerRow.createCell(CELL_TO);
             headerTo.setCellValue(mContext.getString(R.string.main_table_header_to));
-            Cell headerLength = headerRow.createCell(2);
+            Cell headerLength = headerRow.createCell(CELL_LENGHT);
             String distanceTitle = mContext.getString(R.string.distance) + " - " + Options.getOptionValue(Option.CODE_DISTANCE_UNITS);
             headerLength.setCellValue(distanceTitle);
-            Cell headerCompass = headerRow.createCell(3);
+            Cell headerCompass = headerRow.createCell(CELL_AZIMUTH);
             String azimuthTitle = mContext.getString(R.string.azimuth) + " - " + Options.getOptionValue(Option.CODE_AZIMUTH_UNITS);
             headerCompass.setCellValue(azimuthTitle);
-            Cell headerClinometer = headerRow.createCell(4);
+            Cell headerClinometer = headerRow.createCell(CELL_SLOPE);
             String clinometerTitle = mContext.getString(R.string.slope) + " - " + Options.getOptionValue(Option.CODE_SLOPE_UNITS);
             headerClinometer.setCellValue(clinometerTitle);
-            Cell headerLeft = headerRow.createCell(5);
+            Cell headerLeft = headerRow.createCell(CELL_LEFT);
             headerLeft.setCellValue(mContext.getString(R.string.left));
-            Cell headerRight = headerRow.createCell(6);
+            Cell headerRight = headerRow.createCell(CELL_RIGHT);
             headerRight.setCellValue(mContext.getString(R.string.right));
-            Cell headerUp = headerRow.createCell(7);
+            Cell headerUp = headerRow.createCell(CELL_UP);
             headerUp.setCellValue(mContext.getString(R.string.up));
-            Cell headerDown = headerRow.createCell(8);
+            Cell headerDown = headerRow.createCell(CELL_DOWN);
             headerDown.setCellValue(mContext.getString(R.string.down));
-            Cell headerNote = headerRow.createCell(9);
+            Cell headerNote = headerRow.createCell(CELL_NOTE);
             headerNote.setCellValue(mContext.getString(R.string.main_table_header_note));
-            Cell headerDrawing = headerRow.createCell(10);
+
+            Cell gpsLatitude = headerRow.createCell(CELL_LATITUDE);//gps_latitude
+            gpsLatitude.setCellValue(mContext.getString(R.string.gps_latitude));
+            Cell gpsLongitude = headerRow.createCell(CELL_LONGITUDE);
+            gpsLongitude.setCellValue(mContext.getString(R.string.gps_longitude));
+            Cell gpsAltitude = headerRow.createCell(CELL_ALTTITUDE);
+            gpsAltitude.setCellValue(mContext.getString(R.string.gps_altitude));
+            Cell gpsAccuracy = headerRow.createCell(CELL_ACCURACY);
+            gpsAccuracy.setCellValue(mContext.getString(R.string.gps_accuracy));
+            
+            Cell headerDrawing = headerRow.createCell(CELL_DRAWING);
             headerDrawing.setCellValue(mContext.getString(R.string.main_table_header_drawing));
-            Cell headerGPS = headerRow.createCell(11);
-            headerGPS.setCellValue(mContext.getString(R.string.main_table_header_gps));
-            Cell headerPhoto = headerRow.createCell(12);
+            Cell headerPhoto = headerRow.createCell(CELL_PHOTO);
             headerPhoto.setCellValue(mContext.getString(R.string.main_table_header_photo));
 
             // Create the drawing patriarch.  This is the top level container for all shapes.
 //            Drawing drawingPatriarch = sheet.createDrawingPatriarch();
-//            CreationHelper helper = wb.getCreationHelper();
+            CreationHelper helper = wb.getCreationHelper();
 
             // legs
             List<Leg> legs = DaoUtil.getCurrProjectLegs();
@@ -104,7 +129,7 @@ public class ExcelExport {
 
                 Row legRow = sheet.createRow(rowCounter);
 
-                Cell from = legRow.createCell(0);
+                Cell from = legRow.createCell(CELL_FROM);
                 Point fromPoint = l.getFromPoint();
                 DaoUtil.refreshPoint(fromPoint);
 
@@ -116,39 +141,39 @@ public class ExcelExport {
                     from.setCellValue(galleryNames.get(prevGalleryId) + fromPoint.getName());
                 }
 
-                Cell to = legRow.createCell(1);
+                Cell to = legRow.createCell(CELL_TO);
                 Point toPoint = l.getToPoint();
                 DaoUtil.refreshPoint(toPoint);
                 to.setCellValue(galleryNames.get(l.getGalleryId()) + toPoint.getName());
-                Cell length = legRow.createCell(2);
+                Cell length = legRow.createCell(CELL_LENGHT);
                 if (l.getDistance() != null) {
                     length.setCellValue(StringUtils.floatToLabel(l.getDistance()));
                 }
-                Cell compass = legRow.createCell(3);
+                Cell compass = legRow.createCell(CELL_AZIMUTH);
                 if (l.getAzimuth() != null) {
                     compass.setCellValue(StringUtils.floatToLabel(l.getAzimuth()));
                 }
-                Cell clinometer = legRow.createCell(4);
+                Cell clinometer = legRow.createCell(CELL_SLOPE);
                 if (l.getSlope() != null) {
                     clinometer.setCellValue(StringUtils.floatToLabel(l.getSlope()));
                 }
-                Cell left = legRow.createCell(5);
+                Cell left = legRow.createCell(CELL_LEFT);
                 if (l.getLeft() != null) {
                     left.setCellValue(StringUtils.floatToLabel(l.getLeft()));
                 }
-                Cell right = legRow.createCell(6);
+                Cell right = legRow.createCell(CELL_RIGHT);
                 if (l.getRight() != null) {
                     right.setCellValue(StringUtils.floatToLabel(l.getRight()));
                 }
-                Cell up = legRow.createCell(7);
+                Cell up = legRow.createCell(CELL_UP);
                 if (l.getTop() != null) {
                     up.setCellValue(StringUtils.floatToLabel(l.getTop()));
                 }
-                Cell down = legRow.createCell(8);
+                Cell down = legRow.createCell(CELL_DOWN);
                 if (l.getDown() != null) {
                     down.setCellValue(StringUtils.floatToLabel(l.getDown()));
                 }
-                Cell note = legRow.createCell(9);
+                Cell note = legRow.createCell(CELL_NOTE);
 
                 Note n = DaoUtil.getActiveLegNote(l);
                 if (n != null) {
@@ -156,52 +181,47 @@ public class ExcelExport {
                 }
 
                 lastGalleryId = l.getGalleryId();
-
-                /*QueryBuilder<Sketch, Integer> queryBuilderSketch = mWorkspace.getDBHelper().getSketchDao().queryBuilder();
-                queryBuilderSketch.where().eq(Sketch.COLUMN_POINT_ID, fromPoint.getId());
-                Sketch existingDrawing = (Sketch) mWorkspace.getDBHelper().getSketchDao().queryForFirst(queryBuilderSketch.prepare());
-                if (null != existingDrawing) {
-                    savePNGResource(resourcesExportFolder, l, existingDrawing.getBitmap());
-                *//*    Log.i(Constants.LOG_TAG_SERVICE, "Exporting drawing");
-                    //add a picture shape
-                    ClientAnchor anchor = helper.createClientAnchor();
-
-                    int pictureIdx = wb.addPicture(existingDrawing.getBitmap(), Workbook.PICTURE_TYPE_PNG);
-
-                    //set top-left corner of the picture,
-                    //subsequent call of Picture#resize() will operate relative to it
-                    anchor.setCol1(9);
-//                    anchor.setCol2(12);
-//                    anchor.setRow1(rowCounter);
-//                    anchor.setRow2(rowCounter + 3);
-                    Picture pict = drawingPatriarch.createPicture(anchor, pictureIdx);
-
-                    //auto-size picture relative to its top-left corner
-//                    pict.resize();*//*
-                }*/
-
-                // TODO export GPS
-
-//                QueryBuilder<Photo, Integer> queryBuilderPhoto = mWorkspace.getDBHelper().getPhotoDao().queryBuilder();
-//                queryBuilderPhoto.where().eq(Sketch.COLUMN_POINT_ID, fromPoint.getId());
-//                Photo existingPoint = (Photo) mWorkspace.getDBHelper().getPhotoDao().queryForFirst(queryBuilderPhoto.prepare());
-//                if (null != existingPoint) {
-//                    Log.i(Constants.LOG_TAG_SERVICE, "Exporting photo");
-//                    saveJPEGResource(resourcesExportFolder, l, existingPoint.getPictureBytes());
-//                    /*//add a picture shape
-//                    ClientAnchor anchor = helper.createClientAnchor();
-//
-//                    int pictureIdx = wb.addPicture(existingPoint.getPictureBytes(), Workbook.PICTURE_TYPE_JPEG);
-//
-//                    //set top-left corner of the picture,
-//                    //subsequent call of Picture#resize() will operate relative to it
-//                    anchor.setCol1(13);
-//                    anchor.setCol2(15);
-//                    anchor.setRow1(rowCounter);
-//                    anchor.setRow2(rowCounter + 3);
-//                    drawingPatriarch.createPicture(anchor, pictureIdx);*/
-//
-//                }
+                
+                // location
+                Location location = DaoUtil.getLocationByPoint(fromPoint);
+                if (location != null){
+                    Cell latitude = legRow.createCell(CELL_LATITUDE);
+                    latitude.setCellValue(LocationUtil.formatLatitude(location.getLatitude()));
+                    Cell longitude = legRow.createCell(CELL_LONGITUDE);
+                    longitude.setCellValue(LocationUtil.formatLongitude(location.getLongitude()));
+                    Cell altitude = legRow.createCell(CELL_ALTTITUDE);
+                    altitude.setCellValue(location.getAltitude());
+                    Cell accuracy = legRow.createCell(CELL_ACCURACY);
+                    accuracy.setCellValue(location.getAccuracy());
+                }
+                
+                // sketch
+                Sketch sketch = DaoUtil.getScetchByLeg(l);
+                if (sketch != null){
+                    Hyperlink  fileLink= helper.createHyperlink(HSSFHyperlink.LINK_FILE);
+                    
+                    Cell sketchCell = legRow.createCell(CELL_DRAWING);
+                    String path = sketch.getFSPath();
+                    String name = new File(path).getName();
+                    
+                    fileLink.setAddress(name);
+                    sketchCell.setCellValue(name);
+                    sketchCell.setHyperlink(fileLink);
+                }
+                
+                // picture
+                Photo photo = DaoUtil.getPhotoByLeg(l);
+                if (photo != null){
+                    Hyperlink  fileLink= helper.createHyperlink(HSSFHyperlink.LINK_FILE);
+                    
+                    Cell photoCell = legRow.createCell(CELL_PHOTO);
+                    String path = photo.getFSPath();
+                    String name = new File(path).getName();
+                    
+                    fileLink.setAddress(name);
+                    photoCell.setCellValue(name);
+                    photoCell.setHyperlink(fileLink);
+                }
             }
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             wb.write(out);
@@ -211,29 +231,5 @@ public class ExcelExport {
             Log.e(Constants.LOG_TAG_SERVICE, "Failed with export", t);
             throw t;
         }
-
-    }
-
-    private void savePNGResource(File resourcesExportFolder, Leg l, byte[] bitmap) throws IOException {
-        saveInternally(resourcesExportFolder, l, bitmap, ".png");
-    }
-
-    private void saveJPEGResource(File resourcesExportFolder, Leg l, byte[] pictureBytes) throws IOException {
-        saveInternally(resourcesExportFolder, l, pictureBytes, ".jpg");
-    }
-
-    private void saveInternally(File exportFolder, Leg l, byte[] content, String extension) throws IOException {
-        if (!exportFolder.exists()) {
-            exportFolder.mkdir();
-        }
-
-        OutputStream out = null;
-        try {
-            out = new FileOutputStream(new File(exportFolder, l.getFromPoint().getName() + extension));
-            IOUtils.write(content, out);
-        } finally {
-            IOUtils.closeQuietly(out);
-        }
-
     }
 }
