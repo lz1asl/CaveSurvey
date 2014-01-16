54de59aaf9c3192d9c85e718b9e8b96bddb8a174
Aleksandar Stoev <as@kleverbeast.com>
2014-01-16 23:48:48 +0200

    drawing activity - save button to use standard icon

---
+++ src/test/com/astoev/cave/survey/test/DaoUtilTest.java
@@ -0,0 +1,25 @@
+package test.com.astoev.cave.survey.test;
+import com.astoev.cave.survey.util.DaoUtil;
+
+public class DaoUtilTest extends ApplicationTestCase<DaoUtil> {
+
+    public DaoUtilTest() {
+        super(DaoUtilTest.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void testPreconditions() {
+    }
+
+
+}
