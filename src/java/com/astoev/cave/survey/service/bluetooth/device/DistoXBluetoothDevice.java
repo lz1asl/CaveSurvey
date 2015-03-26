1e0f8ff2e328ee6cd3103d61024cc3dba94adda6
lz1asl <cave.survey.project@gmail.com>
2015-03-26 12:30:37 +0200

    Placeholder for distox device

---
+++ src/java/com/astoev/cave/survey/service/bluetooth/device/DistoXBluetoothDevice.java
@@ -0,0 +1,62 @@
+package com.astoev.cave.survey.service.bluetooth.device;
+
+import com.astoev.cave.survey.Constants;
+import com.astoev.cave.survey.exception.DataException;
+import com.astoev.cave.survey.service.bluetooth.Measure;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ * Created by astoev on 3/26/15.
+ */
+public class DistoXBluetoothDevice extends AbstractBluetoothDevice {
+
+
+    @Override
+    public boolean isNameSupported(String aName) {
+        return false;
+    }
+
+    @Override
+    protected String getSPPUUIDString() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return "DistoX";
+    }
+
+    @Override
+    public void triggerMeasures(OutputStream aStream, List<Constants.MeasureTypes> aMeasures) throws IOException {
+        // TODO
+    }
+
+    @Override
+    public void configure(InputStream anInput, OutputStream anOutput) throws IOException {
+
+    }
+
+    @Override
+    public List<Measure> decodeMeasure(byte[] aResponseBytes, List<Constants.MeasureTypes> aMeasures) throws IOException, DataException {
+        return null;
+    }
+
+    @Override
+    public boolean isPassiveBTConnection() {
+        return false;
+    }
+
+    @Override
+    public boolean isMeasureSupported(Constants.MeasureTypes aMeasureType) {
+        return false;
+    }
+
+    @Override
+    public boolean isFullPacketAvailable(byte[] aBytesBuffer) {
+        return false;
+    }
+}
