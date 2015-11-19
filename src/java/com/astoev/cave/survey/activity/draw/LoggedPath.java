693b184d9ff0afda7e1936a0bf7286e120bbfbea
lz1asl <cave.survey.project@gmail.com>
2015-11-19 14:08:00 +0200

    drawing in sync with moved map

--- src/java/com/astoev/cave/survey/activity/draw/LoggedPath.java
+++ src/java/com/astoev/cave/survey/activity/draw/LoggedPath.java
@@ -2,6 +2,7 @@ package com.astoev.cave.survey.activity.draw;
 
 import android.graphics.Path;
 
+import com.astoev.cave.survey.activity.map.MapView;
 import com.astoev.cave.survey.model.SketchElement;
 import com.astoev.cave.survey.model.SketchPoint;
 
@@ -16,10 +17,23 @@ import java.util.List;
 public class LoggedPath extends Path {
 
     private List<SketchPoint> mPoints = new ArrayList<SketchPoint>();
+    private float mMoveX;
+    private float mMoveY;
+    private Integer mScale;
 
     @Override
     public void lineTo(float aX, float aY) {
-        super.lineTo(aX, aY);
+        // display the line scaled
+        super.lineTo(moveAndScale(mMoveX, aX), moveAndScale(mMoveY, aY));
+        // persist the original line
+        mPoints.add(new SketchPoint(null, aX, aY));
+    }
+
+    @Override
+    public void moveTo(float aX, float aY) {
+        // display the line scaled
+        super.moveTo(moveAndScale(mMoveX, aX), moveAndScale(mMoveY, aY));
+        // persist the original line
         mPoints.add(new SketchPoint(null, aX, aY));
     }
 
@@ -29,19 +43,29 @@ public class LoggedPath extends Path {
 
     public static LoggedPath fromSketchElement(SketchElement anElement, float moveX, float moveY, Integer scale) throws SQLException {
         LoggedPath path = new LoggedPath();
+        path.mMoveX = moveX;
+        path.mMoveY = moveY;
+        path.mScale = scale;
 
         Collection<SketchPoint> points = anElement.getPoints();
         boolean first = true;
-        float realScale = 1;// TODO scale / MapView.INITIAL_SCALE;
         for (SketchPoint point : points) {
             if (first) {
-                path.moveTo(moveX + point.getX() * realScale, moveY + point.getY() * realScale);
+                path.moveTo(point.getX(), point.getY());
                 first = false;
             } else {
-                path.lineTo(moveX + point.getX(), moveY + point.getY());
+                path.lineTo(point.getX(),point.getY());
             }
         }
 
         return path;
+    }
+
+    private float moveAndScale(float aMove, float aCoordinate) {
+        float scale = 1;
+        if (mScale != null) {
+            scale = (mScale/ MapView.INITIAL_SCALE);
+        }
+        return aMove + aCoordinate * scale;
     }
 }
