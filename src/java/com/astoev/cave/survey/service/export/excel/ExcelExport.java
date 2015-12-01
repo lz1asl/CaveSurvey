eed1a73a8db14d8933dbb10d7dfde4b84d075b50
lz1asl <cave.survey.project@gmail.com>
2015-12-01 07:39:55 +0200

    fix excel export bug caused bu incompatible sheet name

--- src/java/com/astoev/cave/survey/service/export/excel/ExcelExport.java
+++ src/java/com/astoev/cave/survey/service/export/excel/ExcelExport.java
@@ -189,7 +189,13 @@ public class ExcelExport extends AbstractExport {
     }
 
     private Sheet createHeader(String aProjectName, Workbook wb) {
-        Sheet sheet = wb.createSheet(aProjectName);
+        Sheet sheet;
+        try {
+            sheet = wb.createSheet(aProjectName);
+        } catch (IllegalArgumentException iae) {
+            Log.i(Constants.LOG_TAG_SERVICE, "Failed to create sheet with the project name, creating default: " + iae.getMessage());
+            sheet = wb.createSheet();
+        }
         Row headerRow = sheet.createRow(0);
         // header cells
         Cell headerFrom = headerRow.createCell(CELL_FROM);
