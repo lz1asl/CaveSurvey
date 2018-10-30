5049b56df62b27e53a9994d529211051aa0f5642
f3sty <f3sty@festy.org>
2018-10-31 07:38:06 +1100

    Support for Suaoki/Mileseey P7 devices

---
+++ src/java/com/astoev/cave/survey/service/bluetooth/device/ble/mileseey/SuaokiP7BluetoothLeDevice.java
@@ -0,0 +1,18 @@
+package com.astoev.cave.survey.service.bluetooth.device.ble.mileseey;
+
+/**
+ *  Suaoki P7, almost identical to a Mileseey P7
+ */
+
+public class SuaokiP7BluetoothLeDevice extends AbstractMileseeyBluetoothLeDevice {
+
+    @Override
+    public boolean isNameSupported(String aName) {
+        return deviceNameStartsWith(aName, "Suaoki P7");
+    }
+    @Override
+    public String getDescription() {
+        return "Suaoki P7";
+    }
+}
+
