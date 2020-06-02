61e926b19b1169b001a4d59aa11756d4683e1f28
lz1asl <alexander.stoev@gmail.com>
2020-06-02 23:01:27 +0300

    fix button label in tests

--- src/androidTest/java/com/astoev/cave/survey/test/helper/Data.java
+++ src/androidTest/java/com/astoev/cave/survey/test/helper/Data.java
@@ -5,7 +5,6 @@ import com.astoev.cave.survey.util.FileStorageUtil;
 import java.io.File;
 import java.util.List;
 
-import static com.astoev.cave.survey.R.id.info_action_export_vtopo;
 import static com.astoev.cave.survey.R.id.info_action_export_xls;
 import static com.astoev.cave.survey.R.id.info_action_openstopo;
 import static com.astoev.cave.survey.service.export.excel.ExcelExport.EXCEL_FILE_EXTENSION;
@@ -25,7 +24,7 @@ public class Data {
 
     public static void visualTopoExport() {
         openContextMenu();
-        click(info_action_export_vtopo);
+        click("Visual Topo export");
     }
 
     public static void opensTopoExport() {
