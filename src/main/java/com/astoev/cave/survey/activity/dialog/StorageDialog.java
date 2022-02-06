1ae9bd61296b7dca4a2413aecdcad69cf5f1badc
lz1asl <alexander.stoev@gmail.com>
2022-02-06 18:52:02 +0200

    add link to the installation notes

--- src/main/java/com/astoev/cave/survey/activity/dialog/StorageDialog.java
+++ src/main/java/com/astoev/cave/survey/activity/dialog/StorageDialog.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
@@ -18,6 +21,9 @@ import com.astoev.cave.survey.activity.home.SplashActivity;
  * Request home folder permission warning dialog.
  */
 public class StorageDialog extends DialogFragment  {
+
+    public static final String STORAGE_DETAILS_URL = "https://github.com/lz1asl/CaveSurvey/wiki/User-Guide#installation";
+
 
     private SplashActivity parent;
 
@@ -38,6 +44,9 @@ public class StorageDialog extends DialogFragment  {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.storage_dialog, null);
         builder.setView(view);
+
+        TextView storageDetails = view.findViewById(R.id.splashStorageDetails);
+        storageDetails.setOnClickListener( l -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(STORAGE_DETAILS_URL))));
 
         return  builder.create();
     }
